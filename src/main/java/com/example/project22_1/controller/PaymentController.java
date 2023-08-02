34d92127edb0b2df642784148ed757796aa23695
asrytikov <asrytikov@gmail.com>
2023-08-02 20:37:45 +0400

    Project 22_1

--- src/main/java/com/example/project22_1/controller/PaymentController.java
+++ src/main/java/com/example/project22_1/controller/PaymentController.java
@@ -1,2 +1,36 @@
-package com.example.project22_1.controller;public class PaymentController {
+package com.example.project22_1.controller;
+
+import com.example.project22_1.model.Payment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+import java.util.logging.Logger;
+
+@RestController
+public class PaymentController {
+
+    private static Logger logger = Logger.getLogger(PaymentController.class.getName());
+
+    @GetMapping("/test")
+    public String test(){
+        return "test";
+    }
+
+    @PostMapping("/payment")
+    public ResponseEntity<Payment> createPayment(
+            @RequestHeader String requestId,
+            @RequestBody Payment payment){
+        logger.info("Received request with ID " + requestId +
+                " ;Payment Amount: " + payment.getAmount());
+
+        payment.setId(UUID.randomUUID().toString());
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .header("requestId", requestId)
+                .body(payment);
+    }
+
 }
